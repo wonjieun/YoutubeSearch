f68f45ecd898449cc725238ade10afbe59b4fd3b
wonjieun <wonjieunkk@gmail.com>
2018-09-02 18:51:19 +0900

    Event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+// const Component = React.Component;
 
-const SearchBar = () => {
-    return <input /> // React.createElement
-};
+// ES6
+class SearchBar extends Component {
+    render() {
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
 
-// const foo = 5;
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
  // 어플리케이션의 어느 곳에서나 SearchBar 컴포넌트를 불러올 수 있다
 export default SearchBar;
