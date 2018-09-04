296b5d632d45a8140507463cec9f0213af09c782
wonjieun <wonjieunkk@gmail.com>
2018-09-04 14:36:00 +0900

    I can watch Youtube. Updating to check null props

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import ReactDOM from 'react-dom';
 import search from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDkNmNdT5v1ppeAsfa3YVndECokBx09QnE';
 
 
@@ -47,6 +48,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
