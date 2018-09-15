9933b5de05258ac30df1ec82545f5249bc70b183
wonjieun <wonjieunkk@gmail.com>
2018-09-15 15:31:00 +0900

    Studying REDUCE

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
+// 앱과 reducer를 연결
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
 
+// state를 mapping한다.
 const rootReducer = combineReducers({
-  // key- state name, value- reducer 그 자체
+  // state: (state = {}) => state
+  // key- state 이름, value- reducer 그 자체
   books: BooksReducer,
   activeBook: ActiveBook
 });
