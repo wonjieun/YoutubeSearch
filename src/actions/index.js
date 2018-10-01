34f21d77db0105b7b1e0cde909c7fa400fde5bdc
wonjieun <wonjieunkk@gmail.com>
2018-10-02 00:18:51 +0900

    mistake pull

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export function selectBook(book) {
-    // console.log('A book has been selected: ', book.title);
-    // selectBook is an ActionCreater, it needs to return an action,
-    // an object with a type property.
-    return {
-        type: 'BOOK_SELECTED',
-        payload: book
-    };
-}
--- src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-// 파일을 import하는 프로젝트는 자동적으로 books reducer를 가져온다
-export default function() {
-    return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Ruby' }
-    ]
-}
